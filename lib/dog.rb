8117c3464d1b6c907da394f699bf98a9cb677dea
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-11 02:04:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+
+  def name=(dogname)
+    @dogname=name
+  end
+  
+  def name
+    @dogname
+  end
+  
+  def bark
+    puts "woof!"
+  end
+
+end
+
+fido=Dog.new
+fido.name="Fido"
